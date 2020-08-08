3e2f6bdb5e0fa41d178dbbb397827e98f088fd97
Kenny Deng <kennydeng352@gmail.com>
2020-08-08 12:23:09 -0400

    A failed attempt to capture context

--- reference/prompt-loop.js
+++ reference/prompt-loop.js
@@ -22,25 +22,32 @@ function promptInput(q) {
 	let done = false;
 	while (!done) {
 		let cmd = await promptInput("Enter a command: ");
+		let context = {stopScript: false};
+
 		if (cmd === "quit") {
 			done = true;
 			rl.close();
 			console.log("Good bye!");
 		} else {
-			try {
-				if (cmd === "") {
-					cmd = "test.js";
-				}
-				console.log("Processing cmd: ", cmd);
-				const args = cmd.split(" ");
-				const fname = args[0];
-				const data = await fs.promises.readFile(fname, "utf8");
+			if (cmd === "stopScript") {
+				console.log("Set stopScript to true");
+				context.stopScript = true;
+			} else {
+				try {
+					if (cmd === "") {
+						cmd = "test.js";
+					}
+					const args = cmd.split(" ");
+					const fname = args[0];
+					console.log("Executing script: ", fname, args);
+					const data = await fs.promises.readFile(fname, "utf8");
 
-				// console.log(data);
-				const func = new Function("require", "args", data);
-				func(require, args.slice(1));
-			} catch (err) {
-				console.log("ERROR: failed to exe function: ", err);
+					// console.log(data);
+					const func = new Function("require", "args", "context", data);
+					func(require, args.slice(1), context);
+				} catch (err) {
+					console.log("ERROR: failed to exe function: ", err);
+				}
 			}
 		}
 	}
