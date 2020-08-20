79c242035832775481a914103353e82c08dfceb0
Kenny Deng <kennydeng352@gmail.com>
2020-08-20 18:45:39 -0400

    Remove unstarted project

--- selenium/projects/ping-pong/get-data.js
+++
@@ -1,7 +0,0 @@
-const {By, until} = require('selenium-webdriver');
-//GET THINGS WORKING BEFORE GOING FOR EFFICIENCY
-(async function main() {
-	await driver.get('https://www.megaspin.net/');
-	const products = await driver.wait(until.elementLocated(By.css('.menu > li > a')));
-	console.log(products);
-})();
